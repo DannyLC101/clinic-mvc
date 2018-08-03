239e930769b8eb4c4651552ba03c3e1d931e6146
Dhawani <dhawani.patel197@gmail.com>
2018-08-03 18:30:58 -0500

    Initial commit

---
+++ src/main/java/org/launchcode/clinicmvc/ClinicMvcApplication.java
@@ -0,0 +1,12 @@
+package org.launchcode.clinicmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicMvcApplication.class, args);
+	}
+}
